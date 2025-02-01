06c142ebdc0ddaee1bdaff69f346c688fb798506
HannanDeveloper1 <hannandeveloper1@gmail.com>
2025-02-01 08:59:13 +0500

    Added Logs Functionality

--- api/src/app.js
+++ api/src/app.js
@@ -1,9 +1,18 @@
 import express from 'express'
 import cors from 'cors'
+import morgan from 'morgan';
+
+import errorMiddleware from "./middlewares/error.middleware.js";
+import logger from './lib/error/winston.js'
+import ErrorHandler from "./lib/error/ErrorHandler.js";
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(morgan('combined', { stream: { write: message => logger.info(message.trim()) } }));
 
 export default app;
+
+app.use(errorMiddleware);
